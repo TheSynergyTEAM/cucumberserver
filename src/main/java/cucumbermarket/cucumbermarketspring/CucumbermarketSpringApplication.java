b928dbab166d98d2af25aa81cd5fea28176a7229
HyungJun Goo <9h0jun1115@gmail.com>
2021-03-11 21:36:38 +0900

    first init

---
+++ src/main/java/cucumbermarket/cucumbermarketspring/CucumbermarketSpringApplication.java
@@ -0,0 +1,13 @@
+package cucumbermarket.cucumbermarketspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CucumbermarketSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CucumbermarketSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cucumbermarket/cucumbermarketspring/CucumbermarketSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package cucumbermarket.cucumbermarketspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CucumbermarketSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
