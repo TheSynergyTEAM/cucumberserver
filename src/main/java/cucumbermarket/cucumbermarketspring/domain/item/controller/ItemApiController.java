882029078bf371f30646b5e14320c885302b0d12
yu-jin-song <yujinsong.tech@gmail.com>
2021-04-01 22:11:35 +0900

    fix: Item controller and service

--- src/main/java/cucumbermarket/cucumbermarketspring/domain/item/controller/ItemApiController.java
+++ src/main/java/cucumbermarket/cucumbermarketspring/domain/item/controller/ItemApiController.java
@@ -1,16 +1,12 @@
 package cucumbermarket.cucumbermarketspring.domain.item.controller;
 
-import cucumbermarket.cucumbermarketspring.domain.item.dto.ItemCreateRequestDto;
-import cucumbermarket.cucumbermarketspring.domain.item.dto.ItemResponseDto;
-import cucumbermarket.cucumbermarketspring.domain.item.dto.ItemUpdateRequestDto;
-import cucumbermarket.cucumbermarketspring.domain.item.service.ItemService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
 public class ItemApiController {
-    private final ItemService itemService;
+    /*private final ItemService itemService;
 
     @PostMapping("/api/item")
     public Long save(@RequestBody ItemCreateRequestDto requestDto){
@@ -36,6 +32,6 @@ public class ItemApiController {
     @GetMapping("/item/{id}")
     public ItemResponseDto findAll(@PathVariable Long id){
         return itemService.findOne(id);
-    }
+    }*/
 
 }
