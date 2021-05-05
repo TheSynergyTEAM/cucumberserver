b05d3c571072b285e868d9a4eee61af7b71876c5
HyungJun <9h0jun1115@gmail.com>
2021-05-05 21:21:26 +0900

    Update AddressRequestController.java
    Refactor

--- src/main/java/cucumbermarket/cucumbermarketspring/domain/member/address/AddressRequestController.java
+++ src/main/java/cucumbermarket/cucumbermarketspring/domain/member/address/AddressRequestController.java
@@ -1,11 +1,7 @@
 package cucumbermarket.cucumbermarketspring.domain.member.address;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @ResponseBody
