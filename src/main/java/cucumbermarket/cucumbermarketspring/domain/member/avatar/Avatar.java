ef52ab7f364e710b2e4bde86d4df7b8b09bfaedf
HyungJun Goo <9h0jun1115@gmail.com>
2021-06-27 23:27:40 +0900

    Feat: Add User Avatar features

---
+++ src/main/java/cucumbermarket/cucumbermarketspring/domain/member/avatar/Avatar.java
@@ -0,0 +1,37 @@
+package cucumbermarket.cucumbermarketspring.domain.member.avatar;
+
+import cucumbermarket.cucumbermarketspring.domain.member.Member;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "avatar")
+@AllArgsConstructor
+@Builder
+@Getter
+public class Avatar {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "avatar_id", unique = true, nullable = false)
+    private Long id;
+
+    @OneToOne(mappedBy = "avatar")
+    private Member member;
+
+    @Column(name = "name")
+    private String name;
+
+    @Lob
+    @Column(name = "bytes")
+    private byte[] bytes;
+
+    @Column(name = "path")
+    private String path;
+    public Avatar() {
+
+    }
+}
